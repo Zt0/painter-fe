ad2133c8b59618bc6df7ef553dcb543ad41464db
Azat Antonyan <azat.antonyan65@gmail.com>
2025-05-19 23:24:26 +0400

    feat: base path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    basePath: '/fe',
     images: {
         domains: ['storage.googleapis.com'], // Add your trusted image domain here
     },
